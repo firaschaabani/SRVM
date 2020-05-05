2846a7953cd81bf4f6d37b2d55bc5851a0994961
Firas <Firas.96.chaabani@gmail.com>
2020-05-05 03:55:00 +0100

    worked on aregistration

--- forms.py
+++ forms.py
@@ -1,17 +1,28 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField,SelectField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+from flask_pymongo import PyMongo  
+from bson.json_util import dumps  
+from bson.objectid import ObjectId   
+from flask import jsonify,request
+from werkzeug.security import generate_password_hash,check_password_hash
+from app import mongo
+
 
 
 class RegistrationForm(FlaskForm):
+    regns=[('Tunis', 'tunis'), ('Ariana', 'ariana'),('Sousse','sousse'),('Kasserine','kasserine')]
     username = StringField('Username',
                            validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password',
-                                     validators=[DataRequired(), EqualTo('password')])
+    
+    region = SelectField(u'Region',choices = regns)
+
+    password = PasswordField('Password', validators=[DataRequired()]) 
     submit = SubmitField('Sign Up')
+    
 
 
 class LoginForm(FlaskForm):
